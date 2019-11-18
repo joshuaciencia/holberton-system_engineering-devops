88e6fa2ddf5fd8b8b18f21408844f85faedb5006
josh <joshuaciencia2016@gmail.com>
2019-11-18 17:58:40 +0000

    task 100 textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(//).join + "," + ARGV[0].scan(//).join + "," + ARGV[0].scan(//).join
+sender = ARGV[0].scan(/\bfrom:.*?\]/).join
+receiver = ARGV[0].scan(/\bto:.*?\]/).join
+flags = ARGV[0].scan(/\bflags:.*?\]/).join
+puts sender[0..-2][5..-1] + "," + receiver[0..-2][3..-1] + "," + flags[0..-2][6..-1]
