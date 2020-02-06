f81429a7ea96e9f0eefd480cdf76a2c6442a87f4
joshua <joshuaciencia2016@gmail.com>
2020-02-06 21:27:43 +0000

    task_2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,22 +6,20 @@ all hot articles for a given subreddit
 import requests as r
 
 
-def recurse(subreddit, hot_list=[], idx=0, after=None):
+def recurse(subreddit, hot_list=[], after=''):
     """ all hot articles of a subreddit """
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
     headers = {
             'User-Agent': 'joshuaciencia'
             }
+    if after is None:
+        return hot_list
     req = r.get(url, headers=headers,
                 allow_redirects=False,
                 params={'after': after})
     if req.status_code != 200:
-        return hot_list
+        return None
     data = req.json().get('data').get('children')
+    hot_list += data
     after = req.json().get('data').get('after')
-    try:
-        title = data[idx].get('data').get('title')
-        hot_list.append(title)
-        return recurse(subreddit, hot_list, idx + 1, after)
-    except IndexError:
-        return recurse(subreddit, hot_list, 0, after)
+    return recurse(subreddit, hot_list, after)
