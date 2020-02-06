c0ba5d47aeefea4462307d60f92bc751c954e795
joshua <joshuaciencia2016@gmail.com>
2020-02-06 20:59:42 +0000

    task-2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+queries the Reddit API and returns the titles
+all hot articles for a given subreddit
+"""
+import requests as r
+
+
+def recurse(subreddit, hot_list=[], idx=0, after=None):
+    """ all hot articles of a subreddit """
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    headers = {
+            'User-Agent': 'joshuaciencia'
+            }
+    req = r.get(url, headers=headers,
+                allow_redirects=False,
+                params={'after': after})
+    if req.status_code != 200:
+        return hot_list
+    data = req.json().get('data').get('children')
+    try:
+        title = data[idx].get('data').get('title')
+        hot_list.append(title)
+        return recurse(subreddit, hot_list, idx + 1, None)
+    except IndexError:
+        after = req.json().get('data').get('after')
+        return recurse(subreddit, hot_list, 0, after)
