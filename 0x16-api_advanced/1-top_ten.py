752ac6ca2eee4ad76174e497039d79c98d694821
joshua <joshuaciencia2016@gmail.com>
2020-02-06 20:17:33 +0000

    task_1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+ueries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit
+"""
+import requests as r
+
+
+def top_ten(subreddit):
+    """ top ten hot posts of a subreddit """
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    headers = {
+            'User-Agent': 'joshuaciencia'
+            }
+    req = r.get(url, headers=headers, allow_redirects=False)
+    if req.status_code != 200:
+        print(None)
+        return
+    data = req.json().get('data').get('children')
+    for d in data[:10]:
+        print(d.get('data').get('title'))
